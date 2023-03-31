30007dea61af028cebaad57e8f90193604671259
yangxing <justlovediaodiao@outlook.com>
2023-03-31 17:44:48 +0800

    update csproj

--- ass/Program.cs
+++ ass/Program.cs
@@ -1,36 +1,38 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
 using System.IO;
-using System.Text;
-using System.Text.Encodings;
 
-namespace Ass
+namespace Ass;
+
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        if (args.Length != 3)
         {
-            if (args.Length != 3)
-            {
-                Console.WriteLine("Usage: Ass <file> <millisecond>");
-                return;
-            }
-            var file = args[1];
-            if(!File.Exists(file)) {
-                Console.WriteLine($"file {file} not found");
-                return;
-            }
-            if (!int.TryParse(args[2], out var millisecond)) {
-                Console.WriteLine("millisecond must be an integer");
-            }
-
-            var ass = new Ass(file);
-            if (millisecond > 0) {
-                ass.Delay(millisecond);
-            } else {
-                ass.Hurry(millisecond);
-            }
-            ass.Save();
+            Console.WriteLine("Usage: Ass <file> <millisecond>");
+            return;
         }
+        var file = args[1];
+        if (!File.Exists(file))
+        {
+            Console.WriteLine($"file {file} not found");
+            return;
+        }
+        if (!int.TryParse(args[2], out var millisecond))
+        {
+            Console.WriteLine("millisecond must be an integer");
+        }
+
+        var ass = new Ass(file);
+        if (millisecond > 0)
+        {
+            ass.Delay(millisecond);
+        }
+        else
+        {
+            ass.Hurry(millisecond);
+        }
+        ass.Save();
     }
 }
