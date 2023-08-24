2579ebe0286f9b779e8e76434b839c72ebe3d626
yangxing <justlovediaodiao@outlook.com>
2023-08-24 10:30:42 +0800

    update sync

--- sync/README.md
+++ sync/README.md
@@ -4,10 +4,17 @@ sync file or text.
 
 ```
 Usage of ./sync:
+ftp
   -d string
         serve directory (default "./")
   -l string
         listen address or port (default "80")
-  -s string
-        service name, ftp or file or text
+file
+  -d string
+        serve directory (default "./")
+  -l string
+        listen address or port (default "80")
+text
+  -l string
+        listen address or port (default "80")
 ```
--- sync/main.go
+++ sync/main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"net"
+	"os"
 	"strings"
 
 	"github.com/justlovediaodiao/tools/sync/file"
@@ -16,43 +17,86 @@ func lanIP() string {
 	if err != nil {
 		return ""
 	}
+	r := "0.0.0.0"
 	for _, address := range addrs {
 		if ipnet, ok := address.(*net.IPNet); ok && ipnet.IP.To4() != nil {
-			// C
-			if ipnet.IP[12] == 192 && ipnet.IP[13] == 168 {
-				return ipnet.IP.String()
-			}
-			// A
-			if ipnet.IP[12] == 10 {
-				return ipnet.IP.String()
-			}
-			// B
-			if ipnet.IP[12] == 172 && ipnet.IP[13] >= 16 && ipnet.IP[13] <= 31 {
-				return ipnet.IP.String()
+			if ipnet.IP[12] == 10 { // A
+				ip := ipnet.IP.String()
+				r = ip
+			} else if ipnet.IP[12] == 172 && ipnet.IP[13] >= 16 && ipnet.IP[13] <= 31 { // B
+				ip := ipnet.IP.String()
+				r = ip
+			} else if ipnet.IP[12] == 192 && ipnet.IP[13] == 168 { // C
+				ip := ipnet.IP.String()
+				return ip // use 192.168.x.x first
 			}
 		}
 	}
-	return "0.0.0.0"
+	return r
+}
+
+type FlagSets map[string]*flag.FlagSet
+
+func (f FlagSets) Add(cmd string) *flag.FlagSet {
+	fs := flag.NewFlagSet(cmd, flag.ExitOnError)
+	f[cmd] = fs
+	return fs
+}
+
+func (f FlagSets) Parse() string {
+	var cmd string
+	if len(os.Args) > 1 {
+		cmd = os.Args[1]
+	}
+	fs, ok := f[cmd]
+	if !ok {
+		f.Usage()
+		os.Exit(0)
+	}
+	fs.Parse(os.Args[2:])
+	return cmd
+}
+
+func (f FlagSets) Usage() {
+	fmt.Printf("Usage of %s:\n", os.Args[0])
+	for _, fs := range f {
+		fmt.Printf("%s\n", fs.Name())
+		fs.PrintDefaults()
+	}
 }
 
 func main() {
-	var service, listen, dir string
-	flag.StringVar(&service, "s", "", "service name, ftp or file or text")
-	flag.StringVar(&listen, "l", "80", "listen address or port")
-	flag.StringVar(&dir, "d", "./", "serve directory")
-	flag.Parse()
-	if !strings.Contains(listen, ":") {
-		listen = fmt.Sprintf("%s:%s", lanIP(), listen)
-	}
-	switch service {
-	case "ftp":
-		ftp.Serve(listen, dir)
+	var listen, dir string
 
+	root := make(FlagSets)
+	fs := root.Add("ftp")
+	fs.StringVar(&listen, "l", "80", "listen address or port")
+	fs.StringVar(&dir, "d", "./", "serve directory")
+
+	fs = root.Add("file")
+	fs.StringVar(&listen, "l", "80", "listen address or port")
+	fs.StringVar(&dir, "d", "./", "serve directory")
+
+	fs = root.Add("text")
+	fs.StringVar(&listen, "l", "80", "listen address or port")
+
+	cmd := root.Parse()
+
+	f := func(addr string) string {
+		if strings.Contains(addr, ":") {
+			return addr
+		}
+		return fmt.Sprintf("%s:%s", lanIP(), addr)
+	}
+
+	switch cmd {
+	case "ftp":
+		ftp.Serve(f(listen), dir)
 	case "file":
-		file.Serve(listen, dir)
+		file.Serve(f(listen), dir)
 	case "text":
-		text.Serve(listen)
+		text.Serve(f(listen))
 	default:
-		flag.Usage()
+		root.Usage()
 	}
 }
